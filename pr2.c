a6ad901674f74b9692b97193b1a1e5643f1b9f00
siddhigodshelwarbatch2024-25 <siddhigodshelwar1404@gmail.com>
2026-04-21 12:14:01 +0530

    Add files via upload

---
+++ pr2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int a=10;
+	int b=20;
+	int result= a+b;
+
+	printf("sum: %d",result);
+
+	int x=10;
+	int y=30;
+	int diff= x-y;
+	printf("diff: %d" ,diff);
+
+	int multiply = a*b;
+
+	printf("multiply: %d" ,multiply);
+
+	int div = a/b;
+	printf("division: %d" ,div);
+	return 0;
+}  
+	
